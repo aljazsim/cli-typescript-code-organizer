e96335a684d61243f483439b6051b4541bcaf04a
Aljaz Simonic <aljaz.simonic@purple.telstra.com>
2025-01-12 21:43:03 +1000

    Update

--- src/index-helper.ts
+++ src/index-helper.ts
@@ -9,13 +9,13 @@ import { SourceCodeOrganizer } from "./source-code/source-code-organizer.js";
 
 async function organizeSourceCode(sourcesDirectoryPath: string, filePath: string, configuration: Configuration)
 {
-    const include = configuration.files.include.map(fp => getFullPath(joinPath(sourcesDirectoryPath, fp)))
-    const exclude = configuration.files.exclude.map(fp => getFullPath(joinPath(sourcesDirectoryPath, fp)))
+    const include = configuration.files.include.map(fp => joinPath(sourcesDirectoryPath, fp))
+    const exclude = configuration.files.exclude.map(fp => joinPath(sourcesDirectoryPath, fp))
     const filePaths = await glob(include, { ignore: exclude });
 
     filePath = getFullPath(filePath);
 
-    if (filePaths.some(fp => fp === filePath))
+    if (filePaths.map(fp => getFullPath(fp)).some(fp => fp === filePath))
     {
         return await SourceCodeOrganizer.organizeSourceCodeFile(filePath, configuration);
     }
@@ -50,7 +50,7 @@ export function displayHelp()
     console.log("tsco -i                                                                    generates configuration file with default values at ./tsco.json");
     console.log("tsco -o                                                                    organizes TypeScript files in ./ recursively using configuration file at ./tsco.json");
     console.log("tsco -w                                                                    watches ./ recursively and organizes TypeScript files on add or change using configuration file at ./tsco.json");
-    console.log("tsco -i -o -w -c /config.json -s ./src/                                    generates configuration file with default values at ./config.json, organizes TypeScript files in ./src/ recursively using configuration file at ./config.json and keeps monitoring ./src/ for file changes");
+    console.log("tsco -i -o -w -c ./src/tsco.json -s ./src/                                 generates configuration file with default values at ./config.json, organizes TypeScript files in ./src/ recursively using configuration file at ./config.json and keeps monitoring ./src/ for file changes");
 }
 
 export function displayVersion()
@@ -62,12 +62,12 @@ export async function initialize(configurationFilePath: string, configuration: C
 {
     await writeFile(configurationFilePath, JSON.stringify(configuration, null, 4), true);
 
-    console.log(`tsco configuration file created at ${getFullPath(configurationFilePath)}`);
+    console.log(`tsco created configuration file at ${getFullPath(configurationFilePath)}`);
 }
 
 export async function organize(sourcesDirectoryPath: string, configuration: Configuration)
 {
-    console.log("tsco organizing files");
+    console.log(`tsco organizing files in ${sourcesDirectoryPath}`);
 
     const include = configuration.files.include.map(fp => getFullPath(joinPath(sourcesDirectoryPath, fp)))
     const exclude = configuration.files.exclude.map(fp => getFullPath(joinPath(sourcesDirectoryPath, fp)))
@@ -91,7 +91,7 @@ export async function organize(sourcesDirectoryPath: string, configuration: Conf
 
 export function parseCommandLineArguments(commandLineArguments: string[])
 {
-    const defaultSourceDirectoryPath = "./";
+    const defaultSourceDirectoryPath = process.cwd();
     const defaultConfigurationFilePath = "./tsco.json";
     const getArgument = (args: string[], short: string, long: string) =>
     {
@@ -117,7 +117,7 @@ export function parseCommandLineArguments(commandLineArguments: string[])
 
 export async function watch(sourcesDirectoryPath: string, configuration: Configuration)
 {
-    console.log("tsco watching files");
+    console.log(`tsco watching files in ${sourcesDirectoryPath}`);
 
     // watch for file changes
     const watcher = new Watcher(sourcesDirectoryPath, { recursive: true, ignoreInitial: true, debounce: 500, persistent: true });
