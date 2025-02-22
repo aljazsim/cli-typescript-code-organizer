ebc26dd14ad9b6c432f5867ef9e20b4afdd9794a
Aljaz Simonic <36719693+aljazsim@users.noreply.github.com>
2025-02-22 20:47:57 +1000

    Fix issue with type only imports
    Co-authored-by: Aljaz Simonic <aljaz.simonic@purple.telstra.com>

--- test/organize-files/special-cases/import-with-type/default-configuration-without-sorting-imports-by-source.ts
+++ test/organize-files/special-cases/import-with-type/default-configuration-without-sorting-imports-by-source.ts
@@ -1,10 +1,15 @@
 import { DateTime, type Duration } from "luxon";
+import type { A, B } from "@c/api";
+import { type C, D } from "@c/api/v2";
+import type { E } from "@c/api/v3";
 import { v4 as uuidv4 } from "uuid";
 
 export function gen(): string
 {
     return uuidv4();
 }
+
+export function sum(a: A, b: B, c: C, d: D, e: E) { }
 
 export function test(date: DateTime): Duration
 {
