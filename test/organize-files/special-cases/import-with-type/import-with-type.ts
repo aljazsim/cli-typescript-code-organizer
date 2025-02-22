54515194f5b6e2aef9801976fee3fabe883e8619
Aljaz Simonic <aljaz.simonic@purple.telstra.com>
2025-02-22 20:46:30 +1000

    Fix issue with type only imports

--- test/organize-files/special-cases/import-with-type/import-with-type.ts
+++ test/organize-files/special-cases/import-with-type/import-with-type.ts
@@ -1,14 +1,24 @@
-import { DateTime, type Duration } from 'luxon';
-import { v4 as uuidv4 } from 'uuid';
+import { DateTime, type Duration } from "luxon";
+import { v4 as uuidv4 } from "uuid";
+
+import { A, type B } from "@c/api";
+import { C, D } from "@c/api/v2";
+import { type E } from "@c/api/v3";
+
+// #region Exported Functions (3)
+
+export function gen(): string
+{
+    return uuidv4();
+}
+
+export function sum(a: A, b: B, c: C, d: D, e: E) { }
 
 export function test(date: DateTime): Duration
 {
     return date.diffNow('day');
 }
 
-export function gen(): string
-{
-    return uuidv4();
-}
+// #endregion Exported Functions
 
 test(DateTime.fromISO('2016-05-25T09:08:34.123+06:00'));
