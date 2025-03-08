5c5c30680ac26a8700c4118d3fe4c98d481085cc
Aljaz Simonic <aljaz.simonic@purple.telstra.com>
2025-03-08 18:20:06 +1000

    Update

--- test/organize-files/special-cases/import-with-type/default-configuration-with-expanding-imports-if-more-than-one.ts
+++ test/organize-files/special-cases/import-with-type/default-configuration-with-expanding-imports-if-more-than-one.ts
@@ -19,11 +19,23 @@ import {
 } from "luxon";
 import { v4 as uuidv4 } from "uuid";
 
+import * as defaultConfigurationWithRegions from "../file-header/default-configuration-with-regions.ts";
+import * as defaultConfigurationWithRegionMemberCount from "./default-configuration-with-region-member-count";
+import * as defaultConfiguration from "./default-configuration.js";
 import * as file1 from "./file/file1";
 
 import { default as data } from "../data.json";
 import { Hash } from "./crypto/hash";
 import { file2 } from "./file/file2";
+
+function getConfigurations()
+{
+    return [
+        defaultConfiguration,
+        defaultConfigurationWithRegions,
+        defaultConfigurationWithRegionMemberCount
+    ];
+}
 
 export function gen(file1: file1, file2: file2): Hash
 {
--- test/organize-files/special-cases/import-with-type/default-configuration-without-separating-import-groups.ts
+++ test/organize-files/special-cases/import-with-type/default-configuration-without-separating-import-groups.ts
@@ -6,10 +6,22 @@ import { type C, D } from "@c/api/v2";
 import type { E } from "@c/api/v3";
 import { DateTime, type Duration } from "luxon";
 import { v4 as uuidv4 } from "uuid";
+import * as defaultConfigurationWithRegions from "../file-header/default-configuration-with-regions.ts";
+import * as defaultConfigurationWithRegionMemberCount from "./default-configuration-with-region-member-count";
+import * as defaultConfiguration from "./default-configuration.js";
 import * as file1 from "./file/file1";
 import { default as data } from "../data.json";
 import { Hash } from "./crypto/hash";
 import { file2 } from "./file/file2";
+
+function getConfigurations()
+{
+    return [
+        defaultConfiguration,
+        defaultConfigurationWithRegions,
+        defaultConfigurationWithRegionMemberCount
+    ];
+}
 
 export function gen(file1: file1, file2: file2): Hash
 {
