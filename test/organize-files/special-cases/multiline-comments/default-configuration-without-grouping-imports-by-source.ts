e73e3fd5c414ae1952016696586d020997fc824a
Aljaz Simonic <aljaz.simonic@purple.telstra.com>
2025-03-08 10:32:52 +1000

    Fix issue with multiline comments

---
+++ test/organize-files/special-cases/multiline-comments/default-configuration-without-grouping-imports-by-source.ts
@@ -0,0 +1,10 @@
+import Admin from "../../admin-variables";
+import App from "../../app-variables";
+import { Selector } from "testcafe";
+
+//! Tests 1 through 16 are not concurrency enabled. Every test after that will be concurrency enabled
+
+// Folders for this Suite
+const folder1 = Admin.Navigation.AdvancedForms as App;
+const folderOption = Admin.getFolderOption(folder1);
+const roleOption = Selector('[id="ZZ Test Cafe-checkbox"]');
