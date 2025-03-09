b308f7a8a3530d3ab5d7b517a5f0822a09d85ac3
Aljaz Simonic <aljaz.simonic@purple.telstra.com>
2025-03-09 17:51:27 +1000

    Update source-code-logger.ts
    Update

--- src/source-code/source-code-logger.ts
+++ src/source-code/source-code-logger.ts
@@ -1,7 +1,23 @@
+// #region Exported Functions (3)
+
+export function log(message: string)
+{
+    logger.log(message);
+}
+
+export function logError(error: string | Error | unknown) 
+{
+    logger.logError(error);
+}
+
 export function setLogger(logging: { log: (message: string) => void, logError: (error: string | Error | unknown) => void })
 {
     logger = logging;
 }
+
+// #endregion Exported Functions
+
+// #region Variables (1)
 
 let logger = {
     log: (message: string) => { console.log(message) },
@@ -16,14 +32,6 @@ let logger = {
             console.error(error)
         }
     }
-}
+};
 
-export function log(message: string)
-{
-    logger.log(message);
-}
-
-export function logError(error: string | Error | unknown) 
-{
-    logger.logError(error);
-}
+// #endregion Variables
